e5c167ff1639e79b86f5141d5fe10f3557768870
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2018-12-04 15:23:53 -0300

    Sexo

--- resources/SexoEst_Resource.py
+++ resources/SexoEst_Resource.py
@@ -1,9 +1,11 @@
+#Fazendo consultas sobre o sexo dos individuos envolvidos nos acidedentes
 from flask_restful import Resource, marshal_with, abort
 from database.PgConector import *
 
 class SexoEst(Resource):
 
     # GET Acidentes
+    #Excecoes do mapa do Brasil, no qual os estados do Acre e de Roraima estao apenas com numeros e nao sua sigla
     def get(self, estado):
         if (estado == "BR.1099"):
             estado = "AC"
@@ -11,7 +13,7 @@ class SexoEst(Resource):
             estado = "RO"
         else:
             estado = estado[3] + estado[4]
-
+        #Consulta SQL
         cur.execute('select sexo, count(sexo) from public."MyData" where uf like \'{}\' group by sexo'.format(estado))
         acidentePesquisa = cur.fetchall()
         #acidentePesquisa = cur.fetchall()[0]
