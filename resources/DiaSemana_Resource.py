8787c2977eccf70ea20ded7f67798b4908d6e616
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2018-12-04 15:15:54 -0300

    DiaSemana

--- resources/DiaSemana_Resource.py
+++ resources/DiaSemana_Resource.py
@@ -1,3 +1,5 @@
+#Fazendo consultas sobre Dia da Semana
+
 from flask_restful import Resource, marshal_with, abort
 from database.PgConector import *
 
@@ -6,7 +8,7 @@ class DiaSemana(Resource):
     # GET Acidentes
     def get(self):
 
-
+        #Consulta SQL
         sql = 'select dia_semana, count(dia_semana) from public."MyData" group by dia_semana'
 
         cur.execute(sql)
