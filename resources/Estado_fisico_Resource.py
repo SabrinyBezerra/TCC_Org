f36470e44388297367303fe7a18257edb1e6492b
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2018-12-04 15:17:47 -0300

    EstadoFisico

--- resources/Estado_fisico_Resource.py
+++ resources/Estado_fisico_Resource.py
@@ -1,3 +1,4 @@
+#Fazendo consultas sobre o Estado Fisico dos individuos envolvidos nos acidentes
 from builtins import print
 
 from flask_restful import Resource, marshal_with, abort
@@ -7,6 +8,7 @@ from database.PgConector import *
 class Estado_fisico_Resource(Resource):
 
     # GET Acidentes
+    #Excecoes do mapa do Brasil, no qual os estados do Acre e de Roraima estao apenas com numeros e nao sua sigla
     def get(self, estado):
         if (estado == "BR.1099"):
             estado = "AC"
@@ -15,6 +17,7 @@ class Estado_fisico_Resource(Resource):
         else:
             estado = estado[3] + estado[4]
 
+         #Consulta SQL
         cur.execute(
             'select estado_fisico, count(estado_fisico) from public."MyData" where uf like \'{}\' group by estado_fisico'.format(estado))
 
