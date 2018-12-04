f8b285896de8fc8787639d675da4ae0cf55b9ca4
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2018-12-04 15:32:13 -0300

    Top5

--- resources/Top_Resource.py
+++ resources/Top_Resource.py
@@ -1,3 +1,4 @@
+#Fazendo consultas sobre os 5 municipios que mais tem acidentes de cada estado
 from builtins import print
 
 from flask_restful import Resource, marshal_with, abort
@@ -6,6 +7,7 @@ from database.PgConector import *
 class Top_Acidente(Resource):
 
     # GET Acidentes
+    #Excecoes do mapa do Brasil, no qual os estados do Acre e de Roraima estao apenas com numeros e nao sua sigla
     def get(self, estado):
         if (estado == "BR.1099"):
             estado = "AC"
@@ -14,6 +16,7 @@ class Top_Acidente(Resource):
         else:
             estado = estado[3] + estado[4]
 
+          #Consulta SQL
         cur.execute(
             'select municipio, count(*) from public."MyData" where uf like \'{}\' group by municipio order by count (*) desc limit 5'.format(estado))
 
