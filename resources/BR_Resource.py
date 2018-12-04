cda0330f6373f2ef36572be42c5bace863d94000
Pedro Henrique Araújo <peedrohenriqueaa@gmail.com>
2018-12-04 15:11:20 -0300

    BR

--- resources/BR_Resource.py
+++ resources/BR_Resource.py
@@ -1,3 +1,4 @@
+#Fazendo consultas de BR
 from builtins import print
 
 from flask_restful import Resource, marshal_with, abort
@@ -6,6 +7,7 @@ from database.PgConector import *
 class BR_Acidente(Resource):
 
         # GET Acidentes
+        #Excecoes do mapa do Brasil, no qual os estados do Acre e de Roraima estao apenas com numeros e nao sua sigla
         def get(self, estado):
             if (estado == "BR.1099"):
                 estado = "AC"
@@ -14,7 +16,7 @@ class BR_Acidente(Resource):
             else:
                 estado = estado[3] + estado[4]
 
-
+        #consulta SQL ao PostgreSQL
             cur.execute(
                 'select BR, count(BR) from public."MyData" where uf like \'{}\' group by BR'.format(estado))
         
